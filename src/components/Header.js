f08eb24185ff670830981abba7daf6630bdce4fe
Helgi <helgidavidsson@proton.megit config --global user.name HelgiHelgigit config --global user.email helgidavidsson@proton.me>
2023-02-16 18:16:07 +0000

    logo text

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,6 @@ function Header() {
                 src="../assets/map-in-color-logo.png"
                 className="logo"
             ></img>
-                        <h1 className="logo-title">MAP IN COLOR</h1>
 
         </div>
     )
